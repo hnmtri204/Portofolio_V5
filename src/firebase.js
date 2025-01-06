f146c6128fa4a0bc6c8e30785f13e2a04b1b5c32
hnmtri204 <hoangngocminhtri.thpt.ht@gmail.com>
2025-01-07 00:43:12 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
+import { addDoc, collection } from "@firebase/firestore"; // Perbarui ini
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui ini
+import { getFirestore } from "firebase/firestore";
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB-lfUt1adpQ0KYcFFW_oAWTJVfHDOOZy8",
-  authDomain: "portofolio-web-3e8e8.firebaseapp.com",
-  projectId: "portofolio-web-3e8e8",
-  storageBucket: "portofolio-web-3e8e8.appspot.com",
-  messagingSenderId: "25195509306",
-  appId: "1:25195509306:web:2b635dcf997137bf612703"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB-lfUt1adpQ0KYcFFW_oAWTJVfHDOOZy8",
+//   authDomain: "portofolio-web-3e8e8.firebaseapp.com",
+//   projectId: "portofolio-web-3e8e8",
+//   storageBucket: "portofolio-web-3e8e8.appspot.com",
+//   messagingSenderId: "25195509306",
+//   appId: "1:25195509306:web:2b635dcf997137bf612703"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, addDoc };
+export { addDoc, collection, db };
